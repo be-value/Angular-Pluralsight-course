e8456438994f608545873b68b9f22805b06f014e
Jelle Be-Value <be-value@live.nl>
2018-04-28 12:04:23 +0200

    Added bootstrap version 3.3.7 to the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pm-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div><H1>{{pageTitle}}</H1>
+      <app-product-list></app-product-list>
+    </div>`,
+  // templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  pageTitle = 'Acme Product Management';
+  pageTitle  = 'Acme Product Management';
 }
